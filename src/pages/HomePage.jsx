dc437b74c76b19cc0317ceed102600ffc7bbcb5c
Jack Calcinius <158483483+calcinius@users.noreply.github.com>
2024-06-18 20:30:42 +0000

    "Update button text color opacity from 30 to 40 in HomePage.jsx"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -191,10 +191,10 @@ function HomePage() {
             </div>
            </header>
            <div className="w-full flex items-center justify-center gap-3 mt-4 md:gap-10 border-b-2 pb-4 border-neutral-900 lg:border-none">
-            <button className={viewType === 0 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/30"} onClick={() => setViewType(0)}>Home</button>
-            <button className={viewType === 1 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/30"} onClick={() => setViewType(1)}>Following</button>
-            <button className={viewType === 2 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/30"} onClick={() => setViewType(2)}>Topics</button>
-            <button className={viewType === 3 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/30"} onClick={() => setViewType(3)}>Trending</button>
+            <button className={viewType === 0 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/40"} onClick={() => setViewType(0)}>Home</button>
+            <button className={viewType === 1 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/40"} onClick={() => setViewType(1)}>Following</button>
+            <button className={viewType === 2 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/40"} onClick={() => setViewType(2)}>Topics</button>
+            <button className={viewType === 3 ? "text-xl transition-colors duration-300 md:text-2xl" : "text-xl transition-colors duration-300 md:text-2xl text-white/40"} onClick={() => setViewType(3)}>Trending</button>
            </div>
            { viewType === 0 && (
            <ul className="w-screen h-full p-4 flex flex-col gap-2 overflow-auto md:w-[650px] [&::-webkit-scrollbar]:w-0">
