493e8ca4fa4d8063843d81aa8e0183884f5ddf80
Jack Calcinius <158483483+calcinius@users.noreply.github.com>
2024-06-18 19:39:07 +0000

    "Update ThoughtItem component: add width constraint to Markdown component"

--- src/components/ThoughtItem.jsx
+++ src/components/ThoughtItem.jsx
@@ -7,7 +7,7 @@ function ThoughtItem({content, title, open, deleteThought}) {
         <div className="w-full border-neutral-900 border-2 flex justify-between items-center gap-3 p-4 md:rounded-md cursor-pointer md:mt-4 mt-2 hover:bg-neutral-950 transition-colors duration-200 rounded-md" >
             <div className="flex flex-col gap-2 text-wrap truncate w-full">
             <h1 className="text-xl truncate hover:underline cursor-pointer" onClick={open}>{title}</h1>
-            <Markdown className="text-md truncate" remarkPlugins={[remarkGfm]} components={{
+            <Markdown className="text-md w-72" remarkPlugins={[remarkGfm]} components={{
               a(props) {
                 const {node, ...rest} = props
                 return <a className="text-blue-500 " href={rest.href} target="_blank">{rest.href}</a>
