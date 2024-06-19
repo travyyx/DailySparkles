27e66f4121a834d35dac2676c628c4c782ff3610
Jack Calcinius <158483483+calcinius@users.noreply.github.com>
2024-06-19 11:43:19 +0000

    "Added onClick event to topic name and removed hover:underline from Markdown component"

--- src/components/TopicsItem.jsx
+++ src/components/TopicsItem.jsx
@@ -14,15 +14,15 @@ function TopicsItem({name, icon, description}) {
         <div className="w-full border-neutral-900 border-2 flex justify-between items-center gap-3 p-2 md:rounded-md cursor-pointer md:mt-4 mt-2 hover:bg-neutral-950 transition-colors duration-200 px-4 rounded flex-col">
                 <div className="flex gap-2 items-center w-full">
                 <img src={icon && icon} alt="topic" className="w-10"/>
-                <h1 className="md:text-xl text-lg">{name && name}</h1>
+                <h1 className="md:text-xl text-lg hover:underline" onClick={MoveToTopic}>{name && name}</h1>
             </div>
             <hr className="w-full border-neutral-800"/>
-            <Markdown className="text-lg hover:underline text-wrap mb-4 md:text-xl text-left w-full" remarkPlugins={[remarkGfm]} components={{
+            <Markdown className="text-lg text-wrap mb-4 md:text-xl text-left w-full" remarkPlugins={[remarkGfm]} components={{
               a(props) {
                 const {node, ...rest} = props
                 return <a className="text-blue-500 " href={rest.href} target="_blank">{rest.href}</a>
               }
-            }} onClick={MoveToTopic}>{description}</Markdown>
+            }}>{description}</Markdown>
         </div>
     )
 }
